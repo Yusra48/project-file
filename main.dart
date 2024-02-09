adc2b828d7eb9d9ee9da0d8e1f82b8aa5827a756
Yusra48 <154058242+Yusra48@users.noreply.github.com>
2024-02-09 13:11:35 -0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,51 +1,77 @@
 import 'dart:io';
 
-// void main() {
-//   for (int num = 1; num <= 100; num++) {
-//     if (num % 3 == 0 && num % 5 == 0) {
-//       print("FizzBuzz");
-//     } else if (num % 3 == 0) {
-//       print("Fizz");
-//     } else if (num % 5 == 0) {
-//       print("Buzz");
-//     } else {
-//       print(num);
-//     }
-//   }
-//   print("Enter a String:");
-//   String inputString = stdin.readLineSync()!;
-//   String reversedString = reverseString(inputString);
-//   print("Reversed String = $reversedString");
-// }
-//   String reverseString(String str) {
-//     List<String> myList = str.split('');
-//     myList = myList.reversed.toList();
-//     return myList.join('');
-//   }
-// void main() {
-//   for (int number = 2; number <= 100; number++) {
-//     if (isPrime(number)) {
-//       print(number);
-//     }
-//   }
-// }
+void main() {
+  printFizzBuzz();
 
-// bool isPrime(int num) {
-//   if (num < 2) {
-//     return false;
-//   }
-//   for (int i = 2; i <= num ~/ 2; i++) {
-//     if (num % i == 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
+
+  print("Enter a String:");
+  String inputString = stdin.readLineSync()!;
+  
+
+  String reversedString = reverseString(inputString);
+  print("Reversed String = $reversedString");
+
+  
+  print("Prime Numbers between 1 and 100:");
+  printPrimeNumbers();
+
+  
+  print("Longest Common Substring:");
+  String str1 = "987098";
+  String str2 = "009876543";
+  String result = longestCommonSubstring(str1, str2);
+  if (result.isNotEmpty) {
+    print("Longest Common Substring: $result");
+  } else {
+    print("No common substring found.");
+  }
+}
+
+void printFizzBuzz() {
+  for (int num = 1; num <= 100; num++) {
+    if (num % 3 == 0 && num % 5 == 0) {
+      print("FizzBuzz");
+    } else if (num % 3 == 0) {
+      print("Fizz");
+    } else if (num % 5 == 0) {
+      print("Buzz");
+    } else {
+      print(num);
+    }
+  }
+}
+
+String reverseString(String str) {
+  List<String> myList = str.split('');
+  myList = myList.reversed.toList();
+  return myList.join('');
+}
+
+void printPrimeNumbers() {
+  for (int number = 2; number <= 100; number++) {
+    if (isPrime(number)) {
+      print(number);
+    }
+  }
+}
+
+bool isPrime(int num) {
+  if (num < 2) {
+    return false;
+  }
+  for (int i = 2; i <= num ~/ 2; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 String longestCommonSubstring(String str1, String str2) {
   int m = str1.length;
   int n = str2.length;
-  List<List<int>> dp =
-      List.generate(m + 1, (i) => List<int>.filled(n + 1, 0), growable: false);
+  List<List<int>> dp = List.generate(m + 1, (i) => List<int>.filled(n + 1, 0),
+      growable: false);
 
   int maxLength = 0;
   int endIndex = 0;
@@ -65,17 +91,4 @@ String longestCommonSubstring(String str1, String str2) {
   }
 
   return str1.substring(endIndex - maxLength + 1, endIndex + 1);
-}
-
-void main() {
-  String str1 = "987098";
-  String str2 = "009876543";
-
-  String result = longestCommonSubstring(str1, str2);
-
-  if (result.isNotEmpty) {
-    print("Longest Common Substring: $result");
-  } else {
-    print("No common substring found.");
-  }
 }
